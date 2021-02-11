3ffdf51d9167cd4fc09b9040d10c7b43375779de
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-11 14:16:30 +0100

    Update Main_2.java
    changed

--- src/Main_2.java
+++ src/Main_2.java
@@ -1,13 +1,13 @@
 import java.util.function.BiFunction;
-class Multiplication{
+class Multiplications{
     public static int divide(int a, int b){
         return a/b;
     }
 }
 public class Main_2 {
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> product = Multiplication::divide;
+        BiFunction<Integer, Integer, Integer> product = Multiplications::divide;
         int vysledok = product.apply(50, 5);
-        System.out.println("Výsledok delenia je: "+vysledok);
+        System.out.print("Výsledok delenjaje: "+vysledok);
     }
 }
