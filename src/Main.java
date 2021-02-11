8965af858f21a6b5c3983c1dff84b3b7d133ec27
jakubvasinsky <jakub.vasinsky@gmail.com>
2021-02-11 13:58:40 +0100

    changed passing function as parameter

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
-interface Callable {
-    public void call(int param);
+
+
+@FunctionalInterface
+interface theInterface{
+    void show();
 }
-
-class Test implements Callable {
-    public void call(int param) {
-        System.out.println( param );
+public class Main {
+    public void myMessage(){
+        System.out.println(">>> Example message <<<");
     }
-}
-
-public class Main{
-    public static void invoke(Callable callable, int param){
-        callable.call(param);
-    }
-    public static void main(String []args){
-        Callable cmd = new Test();
-        invoke(cmd, 10);
-
+    public static void main(String[] args) {
+        Main obj = new Main();
+        theInterface ref = obj::myMessage;
+        ref.show();
     }
 }
